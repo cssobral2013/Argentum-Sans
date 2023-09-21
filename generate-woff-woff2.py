73a99442408c9228bbf2abaf6b805a600ab38b7f
Cristiano Sobral <cssobral2013@gmail.com>
2023-09-21 20:11:57 -0300

    Update v3.16

---
+++ generate-woff-woff2.py
@@ -0,0 +1,333 @@
+from fontTools.ttLib import TTFont
+
+'''Generate WOFF from TTF or OTF font.'''
+
+srcPath  = './ttf/ArgentumSans-Thin.ttf'
+woffPath = './web/ArgentumSans-Thin.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+
+srcPath  = './ttf/ArgentumSans-ThinItalic.ttf'
+woffPath = './web/ArgentumSans-ThinItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+
+srcPath  = './ttf/ArgentumSans-ExtraLight.ttf'
+woffPath = './web/ArgentumSans-ExtraLight.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+
+srcPath  = './ttf/ArgentumSans-ExtraLightItalic.ttf'
+woffPath = './web/ArgentumSans-ExtraLightItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+    
+srcPath  = './ttf/ArgentumSans-Light.ttf'
+woffPath = './web/ArgentumSans-Light.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/ArgentumSans-LightItalic.ttf'
+woffPath = './web/ArgentumSans-LightItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/ArgentumSans-LightItalic.ttf'
+woffPath = './web/ArgentumSans-LightItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/ArgentumSans-Regular.ttf'
+woffPath = './web/ArgentumSans-Regular.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/ArgentumSans-Italic.ttf'
+woffPath = './web/ArgentumSans-Italic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/ArgentumSans-Medium.ttf'
+woffPath = './web/ArgentumSans-Medium.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/ArgentumSans-MediumItalic.ttf'
+woffPath = './web/ArgentumSans-MediumItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/ArgentumSans-SemiBold.ttf'
+woffPath = './web/ArgentumSans-SemiBold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/ArgentumSans-SemiBoldItalic.ttf'
+woffPath = './web/ArgentumSans-SemiBoldItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/ArgentumSans-SemiBold.ttf'
+woffPath = './web/ArgentumSans-SemiBold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/ArgentumSans-SemiBoldItalic.ttf'
+woffPath = './web/ArgentumSans-SemiBoldItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/ArgentumSans-Bold.ttf'
+woffPath = './web/ArgentumSans-Bold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/ArgentumSans-BoldItalic.ttf'
+woffPath = './web/ArgentumSans-BoldItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/ArgentumSans-ExtraBold.ttf'
+woffPath = './web/ArgentumSans-ExtraBold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/ArgentumSans-ExtraBoldItalic.ttf'
+woffPath = './web/ArgentumSans-ExtraBoldItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/ArgentumSans-Black.ttf'
+woffPath = './web/ArgentumSans-Black.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/ArgentumSans-BlackItalic.ttf'
+woffPath = './web/ArgentumSans-BlackItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+
+
+'''Generate WOFF2 from TTF or OTF font.'''
+
+srcPath  = './ttf/ArgentumSans-Thin.ttf'
+woff2Path = './web/ArgentumSans-Thin.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)
+
+srcPath  = './ttf/ArgentumSans-ThinItalic.ttf'
+woff2Path = './web/ArgentumSans-ThinItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)
+
+srcPath  = './ttf/ArgentumSans-ExtraLight.ttf'
+woff2Path = './web/ArgentumSans-ExtraLight.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)
+
+srcPath  = './ttf/ArgentumSans-ExtraLightItalic.ttf'
+woff2Path = './web/ArgentumSans-ExtraLightItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)
+    
+srcPath  = './ttf/ArgentumSans-Light.ttf'
+woff2Path = './web/ArgentumSans-Light.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/ArgentumSans-LightItalic.ttf'
+woff2Path = './web/ArgentumSans-LightItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/ArgentumSans-LightItalic.ttf'
+woff2Path = './web/ArgentumSans-LightItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/ArgentumSans-Regular.ttf'
+woff2Path = './web/ArgentumSans-Regular.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/ArgentumSans-Italic.ttf'
+woff2Path = './web/ArgentumSans-Italic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/ArgentumSans-Medium.ttf'
+woff2Path = './web/ArgentumSans-Medium.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/ArgentumSans-MediumItalic.ttf'
+woff2Path = './web/ArgentumSans-MediumItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/ArgentumSans-SemiBold.ttf'
+woff2Path = './web/ArgentumSans-SemiBold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/ArgentumSans-SemiBoldItalic.ttf'
+woff2Path = './web/ArgentumSans-SemiBoldItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/ArgentumSans-SemiBold.ttf'
+woff2Path = './web/ArgentumSans-SemiBold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/ArgentumSans-SemiBoldItalic.ttf'
+woff2Path = './web/ArgentumSans-SemiBoldItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/ArgentumSans-Bold.ttf'
+woff2Path = './web/ArgentumSans-Bold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/ArgentumSans-BoldItalic.ttf'
+woff2Path = './web/ArgentumSans-BoldItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/ArgentumSans-ExtraBold.ttf'
+woff2Path = './web/ArgentumSans-ExtraBold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/ArgentumSans-ExtraBoldItalic.ttf'
+woff2Path = './web/ArgentumSans-ExtraBoldItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/ArgentumSans-Black.ttf'
+woff2Path = './web/ArgentumSans-Black.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/ArgentumSans-BlackItalic.ttf'
+woff2Path = './web/ArgentumSans-BlackItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
